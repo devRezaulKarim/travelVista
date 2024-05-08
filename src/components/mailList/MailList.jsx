b66f8234b4e921efbff7603b1d9642beebe0e793
devRezaulKarim <dev.rezaulkarim@gmail.com>
2024-05-08 15:19:50 -0600

    FIX: Fix responsiveness of mailList

--- src/components/mailList/MailList.jsx
+++ src/components/mailList/MailList.jsx
@@ -8,7 +8,7 @@ const MailList = () => {
       </video>
       <h1 className="mailTitle">Save time, save money!</h1>
       <span className="mailDesc">
-        Sign up and we'll send the best deals to you
+        Sign up and we&#39;ll send the best deals to you
       </span>
       <div className="mailInputContainer">
         <input type="text" placeholder="Your Email" />
